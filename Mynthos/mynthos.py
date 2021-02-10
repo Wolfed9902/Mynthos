1c825dd473fb64b1fee753e7a1829ef00682454f
Dylan Wolfe <wolfed9902@gmail.com>
2021-02-09 19:58:10 -0500

    Revamp tile and draw mechanics

--- Mynthos/mynthos.py
+++ Mynthos/mynthos.py
@@ -1,10 +1,11 @@
-# Mynthos : A World Generator
+# Mynthos : A World Generator (Prototype)
 # By Dylan Wolfe
 # Created October, 2020
 
 #--- Imports ---#
 import pygame
 import random
+import terrain
 
 #--- Init ---#
 pygame.init()
@@ -27,10 +28,12 @@ ocean_alt = (76, 166, 255)
 
 # Game Objects
 tile_size = 6
-land_origin = (random.randint(0,(display_width/4))) * 2
-land_height = (random.randint(land_origin,(display_height/2))) * 2
-land_width = (random.randint(land_origin,(display_height/2))) * 2 # FIX! - land tiles exceed even numbers
-land_clrlist = []
+ttile_list = [['X' for y in range(0, int(display_height/tile_size))] for x in range(0, int(display_width/tile_size))] # 2D array that holds terrain
+
+#land_origin = (random.randint(0,(display_width/4))) * 2    ------------- OUTDATED
+#land_height = (random.randint(land_origin,(display_height/2))) * 2
+#land_width = (random.randint(land_origin,(display_height/2))) * 2 # FIX! - land tiles exceed even numbers
+#land_clrlist = []
 
 def draw_back():
     window.fill(bgc)
@@ -42,32 +45,32 @@ def draw_grid():
     for y in range(0, display_height, tile_size):
         pygame.draw.line(window, ln, (0, y), (display_width, y))
 
-def draw_ocean():
+def draw_terrain():
+
+    for j in range (0, int(display_height/tile_size)): # populate ttile_list
+        for i in range(0, int(display_height/tile_size)):
+            ttile_list[i][j] = random.choice(["O","L"])
 
     for y in range (0, display_height, tile_size): # horizontal fill until vertical end of display
         for x in range(0, display_width, tile_size): # create a rect size tile_size until horizontal end of display
-            rcolor = random.choice([ocean,ocean_alt]) # cycle ocean colors
+            #rtype = random.choice(["ocean","barren"]) # cycle terrain type
+            rcolor = random.choice([ocean,barren]) # TEMP
             pygame.draw.rect(window, rcolor, (x, y, tile_size, tile_size))
 
-def draw_land():
-
-    # TEMPORARY - Draw a rect for continent
-    for y in range (land_origin, land_height, tile_size):
-        for x in range (land_origin, land_width, tile_size):
-            rcolor = random.choice([barren,verdant])
-            pygame.draw.rect(window, rcolor, (x, y, tile_size, tile_size))
 
 #--- Runtime ---#
 
-
 run = True
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(200)
 
-    draw_back()
-    draw_grid()
-    draw_ocean()
-    draw_land()
+    while (pygame.time.get_ticks()) < 2000: # Draw once TEMP
+
+        draw_back()
+        draw_grid()
+        draw_terrain()
+
+        print(ttile_list)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
