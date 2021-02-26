2197a732e2ba73fdf2de943e7371c9e8101e53e6
Dylan Wolfe <wolfed9902@gmail.com>
2021-02-26 17:20:16 -0500

    Update terrain.py

--- mynthos/terrain.py
+++ mynthos/terrain.py
@@ -2,14 +2,11 @@
 
 class terrain_tile:
 
-    color = (255, 255, 255)
-
     def __init__(self, type):
-        #self.type = type
-        type_color(type)
+        self.type = type
 
-    def type_color(type):
-        if type == "ocean":
-            self.color = (52, 152, 255)
-        elif type == "barren":
-            self.color = (210, 162, 120)
+    # passable
+
+    # modifiable
+
+    # static_placement
