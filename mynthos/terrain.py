5072b6921f82c1d23e06c0fa1a8176370f409043
Dylan Wolfe <wolfed9902@gmail.com>
2021-04-07 15:34:34 -0400

    Add value .selected to TerrainTile

--- mynthos/terrain.py
+++ mynthos/terrain.py
@@ -5,11 +5,11 @@ class TerrainTile:
     def __init__(self, type):
         self.type = type
         self.passable = True
+        self.selected = False
 
     def __repr__(self):
         return self.type
 
-    # passable
 
     # modifiable
 
