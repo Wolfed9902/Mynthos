82ae1db941442c9491ea0bb23a8408d3d913c258
Dylan Wolfe <wolfed9902@gmail.com>
2021-04-28 15:30:15 -0400

    Add TerrainTile Description

--- mynthos/terrain.py
+++ mynthos/terrain.py
@@ -1,5 +1,8 @@
 # Mynthos Terrain Class
 
+# Description:
+# TerrainTile represents a 'pixel' of terrain on a map, of which size can be designated in settings. Terrain type is equivalent to a 'biome'.
+
 class TerrainTile:
 
     def __init__(self, type):
